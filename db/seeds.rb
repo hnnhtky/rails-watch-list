460fd50a795c6e0a43d9aff59ea39d6b9d35042a
Hannah Lapasaran <hlapasaran@gmail.com>
2025-07-12 15:01:32 +0900

    Done with Lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+require 'open-uri'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+
+puts "Fetching movies from TMDB..."
+url = "https://tmkitt.lewagon.com/db/movie/top_rated"
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)["results"]
+
+puts "Creating movies..."
+movies.first(10).each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Movies created!"
